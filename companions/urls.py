1568df9ddbca1ab679e6381f9a904cf6bf1e24a4
StrikerSam-Ai <shashwatmishra0369@gmail.com>
2025-04-17 14:01:38 +0530

    changes made like working home page etc.

--- companions/urls.py
+++ companions/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login_user, name='login'),
+    path('', views.login_view, name='login'),  # Make login the default landing page
+    path('home/', views.home, name='home'),    # Change home to a different URL
+    path('login/', views.login_user, name='login-user'),  # Keep the API endpoint
     path('register/', views.register_user, name='register-user'),
-    path('api/token/', views.login_user, name='login-user'),
-    path('test/', views.test_view, name='test-view'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
+    path('test/', views.test_view, name='test-view'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -127,15 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'core/static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT_DIR = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
